6cb58f55472e5ec04fc9e7d8711a569d0cdb40e7
AlexSmith716 <AlexSmith716@outlook.com>
2019-08-12 11:18:37 -0400

    chore (package): Updated core-js and file-loader and react-hot-loader and eslint-config-airbnb.

--- src/utils/mockAPI.js
+++ src/utils/mockAPI.js
@@ -131,7 +131,6 @@ function postRequestConcatResolveRejectPromise(dataObj, r, delay) {
   return promise;
 };
 
-// 2nd promise is rejected but being resolved and returned on 1st promise from reducer load action
 // promise.all: rejects with reason of 1st promise that rejects
 // async always return a promise
 // await suspends execution until the promise is settled
@@ -146,14 +145,6 @@ async function doSomeAsyncSyncLikeOperations() {
 
   //console.log(`###### mockAPI > doSomeAsyncSyncLikeOperations > timeElapsedClass1.getSecondsElapsed 1: ${timeElapsedClass1.getSecondsElapsed()}`);
   console.log(`###### mockAPI > doSomeAsyncSyncLikeOperations > timeElapsedModule1.getSecondsElapsed() 1: ${timeElapsedModule1.getSecondsElapsed()}`);
-
-  const startSetTimeoutArrayLong = [];
-  startSetTimeoutArrayLong.push(startResolvedPromise(2500));
-  startSetTimeoutArrayLong.push(startResolvedPromise(22250));
-
-  const startSetTimeoutArrayFast = [];
-  startSetTimeoutArrayFast.push(startResolvedPromise(100));
-  startSetTimeoutArrayFast.push(startResolvedPromise(300));
 
   // -------------------------
 
@@ -171,52 +162,11 @@ async function doSomeAsyncSyncLikeOperations() {
 
   // -------------------------
 
-  // const pAf1 = await Promise.all(startSetTimeoutArrayFast);
-  // console.log('###### mockAPI > doSomeAsyncSyncLikeOperations > startSetTimeoutArrayFast1-0: ', (pAf1[0]-dn) );
-  // console.log('###### mockAPI > doSomeAsyncSyncLikeOperations > startSetTimeoutArrayFast1-1: ', (pAf1[1]-dn) );
-
-  // // -------------------------
-
-  // const pAl = await Promise.all(startSetTimeoutArrayLong);
-  // console.log('###### mockAPI > doSomeAsyncSyncLikeOperations > startSetTimeoutArrayLong1-0: ', (pAl[0]-dn) );
-  // console.log('###### mockAPI > doSomeAsyncSyncLikeOperations > startSetTimeoutArrayLong1-1: ', (pAl[1]-dn) );
-
-  // -------------------------
-
   const b = await startResolvedPromise(100);
   console.log('###### mockAPI > doSomeAsyncSyncLikeOperations > startResolvedPromise(100) > secondsElapsed b: ', b);
 
   const z = await startResolvedPromise(4000);
   console.log('###### mockAPI > doSomeAsyncSyncLikeOperations > startResolvedPromise(4000) > secondsElapsed z: ', z);
-
-  // -------------------------
-
-  // const a = startResolvedRejectedPromise('foober', 500);
-
-  // a.then(
-  //   (val) => { console.log('###### mockAPI > doSomeAsyncSyncLikeOperations > startResolvedRejectedPromise > a > resolve: ', val ) },
-  //   (error) => { console.log('###### mockAPI > doSomeAsyncSyncLikeOperations > startResolvedRejectedPromise > a > reject: ', error ) }
-  // );
-
-  // const aa = startResolvedRejectedPromise('json', 600);
-
-  // aa.then(
-  //   (val) => { console.log('###### mockAPI > doSomeAsyncSyncLikeOperations > startResolvedRejectedPromise > aa > resolve: ', val ) },
-  //   (error) => { console.log('###### mockAPI > doSomeAsyncSyncLikeOperations > startResolvedRejectedPromise > aa > reject: ', error ) }
-  // );
-
-  // const aaa = startResolvedRejectedPromise('fooberrrrr', 15000);
-
-  // aaa.then(
-  //   (val) => { console.log('###### mockAPI > doSomeAsyncSyncLikeOperations > startResolvedRejectedPromise > aaa > resolve: ', val ) },
-  //   (error) => { console.log('###### mockAPI > doSomeAsyncSyncLikeOperations > startResolvedRejectedPromise > aaa > reject: ', error ) }
-  // );
-
-  // -------------------------
-
-  // const pAf2 = await Promise.all(startSetTimeoutArrayFast);
-  // console.log('###### mockAPI > doSomeAsyncSyncLikeOperations > startSetTimeoutArrayFast2-0: ', (pAf2[0]-dn) );
-  // console.log('###### mockAPI > doSomeAsyncSyncLikeOperations > startSetTimeoutArrayFast2-1: ', (pAf2[1]-dn) );
 
   // -------------------------
 
